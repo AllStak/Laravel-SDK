701373c86d5343f7a7d82bb697a8ee1982c68a4f
abdualziz-techsea <a.alosaimi@techsea.sa>
2025-10-01 22:40:11 +0300

    Update AllStakServiceProvider.php

--- src/AllStakServiceProvider.php
+++ src/AllStakServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace AllStak;
 
+use AllStak\Console\Commands\InstallAllStakCommand;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use AllStak\Tracing\DBSpanRecorder;
@@ -12,44 +13,44 @@ class AllStakServiceProvider extends ServiceProvider
     {
         // Register the config file
         $this->mergeConfigFrom(
-            __DIR__.'/../config/AllStak.php', 'allstack'
+            __DIR__.'/../config/allstak.php', 'allstak'
         );
 
         $this->app->singleton(AllStakClient::class, function ($app) {
             $config = $app['config']['allstak'];
-            
+
             return new AllStakClient(
                 $config['api_key'] ?? env('ALLSTAK_API_KEY', ''),
                 $config['environment'] ?? env('ALLSTAK_ENVIRONMENT', app()->environment()),
-                $config["SEND_IP_ADDRESS"] ?? env('ALLSTAK_SEND_IP_ADDRESS', false)
+                $config['SEND_IP_ADDRESS'] ?? env('ALLSTAK_SEND_IP_ADDRESS', false)
             );
         });
 
-        $this->app->alias(AllStakClient::class, 'allstack');
+        $this->app->alias(AllStakClient::class, 'allstak');
     }
 
     public function boot()
     {
         // Publish the config file
         $this->publishes([
-            __DIR__.'/../config/AllStak.php' => config_path('AllStak.php')
-        ], 'allstack-config');
+            __DIR__.'/../config/allstak.php' => config_path('allstak.php')
+        ], 'allstak-config');
 
         // 1. HTTP request span (global middleware)
         $this->app['router']->pushMiddlewareToGroup('web', \AllStak\Middleware\AllStakTracingMiddleware::class);
         $this->app['router']->pushMiddlewareToGroup('api', \AllStak\Middleware\AllStakTracingMiddleware::class);
 
+        // 2. DB query tracing
         $recorder = new DBSpanRecorder(app(AllStakClient::class));
         DB::listen(function ($query) use ($recorder) {
             $recorder->record($query);
         });
 
+        // 3. Register console commands
         if ($this->app->runningInConsole()) {
             $this->commands([
-                \AllStak\Console\Commands\InstallAllStakCommand::class,
+                InstallAllStakCommand::class,
             ]);
         }
-
     }
-
 }
