02fd3852529d47480dd6ba634c75164d44ff7cc2
abdualziz-techsea <a.alosaimi@techsea.sa>
2025-10-02 17:32:08 +0300

    Update DBSpanRecorder.php

--- src/Tracing/DBSpanRecorder.php
+++ src/Tracing/DBSpanRecorder.php
@@ -9,6 +9,7 @@ use AllStak\AllStakClient;
 class DBSpanRecorder
 {
     protected $client;
+    protected static $isRecording = false;
 
     public function __construct(AllStakClient $client)
     {
@@ -17,24 +18,33 @@ class DBSpanRecorder
 
     public function record(QueryExecuted $query)
     {
-        \Log::debug('DBSpanRecorder::record called', ['sql' => $query->sql]);
+        if (self::$isRecording) {
+            return;
+        }
 
-        $span = [
-            'id' => bin2hex(random_bytes(8)),
-            'trace_id' => SpanContext::getTraceId(),
-            'parent_span_id' => SpanContext::getParentSpanId(),
-            'name' => 'db.query',
-            'start_time' => microtime(true) - ($query->time / 1000), // estimate start
-            'end_time' => microtime(true),
-            'attributes' => [
-                'sql' => $query->sql,
-                'bindings' => json_encode($query->bindings),
-                'time_ms' => $query->time,
-                'connection' => $query->connectionName,
-            ],
-            'status' => 'ok',
-        ];
-        $this->client->sendDbSpan($span);
+        self::$isRecording = true;
+
+        try {
+            \Log::debug('DBSpanRecorder::record called', ['sql' => $query->sql]);
+
+            $span = [
+                'id' => bin2hex(random_bytes(8)),
+                'trace_id' => SpanContext::getTraceId(),
+                'parent_span_id' => SpanContext::getParentSpanId(),
+                'name' => 'db.query',
+                'start_time' => microtime(true) - ($query->time / 1000),
+                'end_time' => microtime(true),
+                'attributes' => [
+                    'sql' => $query->sql,
+                    'bindings' => json_encode($query->bindings),
+                    'time_ms' => $query->time,
+                    'connection' => $query->connectionName,
+                ],
+                'status' => 'ok',
+            ];
+            $this->client->sendDbSpan($span);
+        } finally {
+            self::$isRecording = false;
+        }
     }
-
 }
